1917ef8dac5a687e5e96a465c957c43de32d29a1
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-07-16 15:27:57 +0200

    Création du component pour la description des livres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { BookDescriptionComponent } from './book-description/book-description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    BookDescriptionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/book-description/book-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-description',
+  templateUrl: './book-description.component.html',
+  styleUrls: ['./book-description.component.css']
+})
+export class BookDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
