51e183aaa3416a62b523205d9dc02a6f7e75f890
wallas938 <drame.sissako001@gmail.com>
2019-07-23 16:40:45 +0200

    Affichage de la requete de l'utilisateur operationnel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,37 +31,12 @@ export class AppComponent {
   clickHandler() {
 
     this.bookService.queryHandler(this.userQuery.value)
+
+    this.lastQuery = this.userQuery.value
+
     this.userQuery.setValue('')
-
-    /* this.lastQuery = this.userQuery.value
-    this.booksSubscription = this.bookService.getBook(this.userQuery.value).subscribe(
-      books => {
-        this.books = books.items.map(book => {
-          let formatedBook: any = {
-            id: book.id,
-            title: book.volumeInfo.title,
-            price: book.saleInfo.listPrice ? book.saleInfo.listPrice.amount : 5.99 ,
-            currencyCode: book.saleInfo.listPrice ? book.saleInfo.listPrice.currencyCode : "EUR",
-            author: book.volumeInfo.authors,
-            categories: book.volumeInfo.categories,
-            description: book.volumeInfo.description,
-            language: book.volumeInfo.language,
-            thumbnail: book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://www.404.ie/assets/img/logo_blue.png",
-            averageRating: book.volumeInfo.averageRating ? book.volumeInfo.averageRating : "Aucune evaluation est disponible",
-            pageCount: book.volumeInfo.pageCount ? book.volumeInfo.pageCount : "100",
-            isEbook: book.saleInfo.isEbook ? book.saleInfo.isEbook : false,
-          }
-          return formatedBook
-        })
-        console.log(books)
-        window.localStorage.setItem('books', this.bookService.toJSONStringfied(this.books))
-      }
-      )
-    
-
-    this.userQuery.setValue('') */
   }
-
+  
   ngOnDestroy(): void {
     
   }
