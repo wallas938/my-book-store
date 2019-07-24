5637dad2bc1f0ac4b326c15f6ee534c24e763991
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-07-24 12:44:43 +0200

    Ajout et modification du modal pour la description de chaque livre

--- src/app/book-description/book-description.component.ts
+++ src/app/book-description/book-description.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BookService } from '../services/book.service';
+import { CartService } from '../services/cart.service';
+import { Ibook } from '../interfaces/Ibook';
 
 @Component({
   selector: 'app-book-description',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDescriptionComponent implements OnInit {
 
-  constructor() { }
+	booksDesc: Ibook[]
+	  booksDescSubcription: Subscription
+
+  constructor(private bookService: BookService,
+  			private cartService: CartService) { 
+              }
 
   ngOnInit() {
+
+  	this.booksDescSubcription = this.bookService.booksSubject.subscribe(
+      (booksDesc: Ibook[]) => {
+        this.booksDesc = booksDesc
+      }
+    )
+    this.bookService.booksSubjectEmitter()
+    this.cartService.booksCountEmitter()
   }
+    
+    cartFiller(bookId: String) {
+
+    let boughtBook: Ibook
+
+    this.booksDesc.forEach(element => {
+
+      if(element.id === bookId) {
+
+        element.isInCart = true
+  
+        boughtBook = element
+      }
+
+      this.cartService.booksCountEmitter()
+    });
+
+      this.cartService.addToCart(boughtBook)
+
+      this.cartService.booksCountEmitter()
+    }
+
+    retrieveHandler(bookId: String) {
+
+      this.booksDesc.forEach(element => {
+
+        if(element.id === bookId) {
+  
+          element.isInCart = false
+
+        }
+  
+      });
+
+      this.cartService.deleteBookFromMain(bookId)
+
+      this.cartService.booksCountEmitter()
+    } 
 
 }
